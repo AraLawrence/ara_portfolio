eea93a087530832b646929451b36414dfdfa6537
AraLawrence <ara.e.lawrence@gmail.com>
2016-03-16 15:50:30 -0700

    content edits

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -13,16 +13,16 @@ const About = React.createClass({
             <p>But you probably knew that</p>
           </div>
           <div className="about-content">
-            <p>What you may not have known is that before
-            before becoming a  web developer I held positions as
-            varied as welder, program director, and educator. </p>
-            <p>This past has allowed me to develop excellent
-            interpersonal sensibilities, and an aptitude
-            for abstract reasoning and creativity.</p>
-            <p>I’ve always loved to create. Now I do that
-            through code. I do collaborative full stack development
-            with an intent toward elegance in technology and design.</p>
-          <p>I also love to hike, eat, and read.</p>
+            <p>In my past I have held positions calling for a great
+            deal of creativity and precision. As a web developer I
+            employ these skills with an intent toward elegance
+            in technology and design.</p>
+            <p>I am highly capable across the stack, and have experience with
+            frameworks like Node, Rails, and Django. I also have aptitude in many modern
+            JavaScript technologies, including Angular and React.</p>
+            <p>Throughout my life I have always loved to create. Now I do that
+            through code. If you have an exciting project and are looking for a
+            driven full stack developer, I would love to hear from you.</p>
           </div>
         </div>
         <div className="about-img">
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: __dirname,
     filename: 'bundle.js',
-    // publicPath: '/dest/'    
+    // publicPath: '/dest/'
     publicPath: "http://www.aralawrence.com/"
 
   },
