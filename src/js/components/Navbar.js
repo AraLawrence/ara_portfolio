2b9b2652081dc3fa7d47d84a0340bee756362ffe
AraLawrence <ara.e.lawrence@gmail.com>
2016-03-16 16:22:33 -0700

    added safari check

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+console.log(isSafari);
+
 const Navbar = React.createClass({
   render() {
     let style = require('../../styles/navbar.sass');
+    let svgClass;
+    if (isSafari) {
+      svgClass = "svg-contain-safari";
+    } else {
+      svgClass = "svg-contain";
+    };
     return (
       <div style={style} className="navbar">
         <div className="logo">
           <Link to="/"><img src="./assets/images/logo.png" alt="Ara logo"/></Link>
         </div>
         <div className="links">
-          <div className="svg-contain left">
+          <div className={svgClass + " left"}>
             <Link to="/about"><svg width="200" height="61">
               <line className="top" x1="0" y1="0" x2="200" y2="0"/>
               <line className="left" x1="0" y1="200" x2="0" y2="-400"/>
@@ -19,7 +28,7 @@ const Navbar = React.createClass({
 	          </svg></Link>
           <Link to="/about" className="text">ABOUT</Link>
           </div>
-          <div className="svg-contain center">
+          <div className={svgClass + " center"}>
             <Link to="/portfolio"><svg width="200" height="61">
               <line className="top" x1="0" y1="0" x2="200" y2="0"/>
               <line className="left" x1="0" y1="200" x2="0" y2="-400"/>
@@ -28,7 +37,7 @@ const Navbar = React.createClass({
 	          </svg></Link>
           <Link to="/portfolio" className="text">PORTFOLIO</Link>
           </div>
-          <div className="svg-contain right">
+          <div className={svgClass + " right"}>
             <Link to="/contact"><svg width="200" height="61">
               <line className="top" x1="0" y1="0" x2="200" y2="0"/>
               <line className="left" x1="0" y1="200" x2="0" y2="-400"/>
