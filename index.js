4a8e145e7fd45220bfd7b4152db7cf83af56b12c
AraLawrence <ara.e.lawrence@gmail.com>
2016-03-16 14:15:01 -0700

    more cors

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ var transporter = nodemailer.createTransport({
 });
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://www.aralawrence.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.aralawrence.com/*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next();
 });
 
