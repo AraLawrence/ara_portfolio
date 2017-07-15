11ff1a0d4c6fd6d96c8fa952b81a2e36f9f70529
AraLawrence <ara.e.lawrence@gmail.com>
2017-07-15 10:26:25 -0700

    small changes to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const webpackConfig = require('./webpack.config');
 const webpack = require('webpack-stream');
 
 gulp.task('default', () => {
-  return gulp.src('src/js/index.js')
+  gulp.src('src/js/index.js')
     .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('dist/'));
+  gulp.src('src/index.html')
     .pipe(gulp.dest('dist/'));
 });
