ca910ceffb2b1daa3de33edda75b0cb06d2f149d
AraLawrence <ara.e.lawrence@gmail.com>
2017-07-15 15:41:39 -0700

    continued refactoring

--- src/js/components/portfolioComponents/portPage.jsx
+++ src/js/components/portfolioComponents/portPage.jsx
@@ -1,10 +1,11 @@
 import { string, shape } from 'prop-types';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import PortTab from './portTab';
-// import Link from 'react-router-dom';
+import style from '../../../styles/portComponentStyles/portPageStyle.sass';
 
 const PortPage = props => (
-  <div className="port-page-body page-body">
+  <div className="port-page-body page-body" style={style}>
     <PortTab data={props.data} />
     <div className="port-page-img">
       <img src={props.data.gif} alt="Gif of project" />
@@ -14,20 +15,25 @@ const PortPage = props => (
     </div>
     <div className="port-page-links">
       <h1>
-        <i className="fa fa-external-link" />
-        <a href={props.data.links[1]} target="_blank" rel="noopener noreferrer">
-          {props.data.links[0]}
+        <img
+          className="ext-icon"
+          src="./assets/images/external-link.svg"
+          alt="Icon for external link"
+        />
+        <a href={props.data.link.address} target="_blank" rel="noopener noreferrer">
+          {props.data.link.text}
         </a>
       </h1>
-      <h1>
-        <i className="fa fa-github-alt" />
-        <a href={props.data.links[3]} target="_blank" rel="noopener noreferrer">
-          {props.data.links[2]}
-        </a>
-      </h1>
+      {props.data.link.github &&
+        <h1>
+          <a href={props.data.link.github} target="_blank" rel="noopener noreferrer">
+            <img src="./assets/images/github.svg" alt="Github icon" />
+          </a>
+        </h1>
+      }
     </div>
     <div className="arrow">
-      {/* <Link to="/work"><img src="./assets/images/arrow.png" alt="Outline of arrow"/></Link>*/}
+      <Link to="/work"><img src="./assets/images/arrow.png" alt="Outline of arrow" /></Link>
     </div>
   </div>
 );
--- src/js/content/portContent.js
+++ src/js/content/portContent.js
@@ -3,25 +3,28 @@ const content = {
     tab: { title: 'OpenCar', text: 'Application Framework Engineer', color: '#adc700' },
     gif: './assets/images/opencar.jpg',
     text: 'At INRIX - OpenCar I write tests and processes to exercise and build our HTML5 based in vehicle application platform in order to ensure that we ship efficiently and at a high standard of quality. A few accomplishments so far include quickly and skillfully creating an HTML5/CSS3/JavaScript web runtime validation tool, as well as building out automated testing technology to exercise the framework and ensure UI specification compliance. Particular technologies utilized include: ECMA2015, Cucumber, Selenium WebDriver, Node.js, Bash',
-    links: ['opencar.com', 'https://www.opencar.com/', '', ''],
+    link: { address: 'http://inrix.com/industries/automotive/', text: 'INRIX-automotive' },
   },
+
   pennypost: {
     tab: { title: 'Pennypost', text: 'Full stack work on Node/Angular app', color: '#00D5A5' },
     gif: './assets/images/pennypost.png',
     text: 'On Pennypost I did full stack development on their webapp (coming soon), collaborating with a team of several developers. On the backend, I worked on integrating Instagram and Stripe APIs and enabling those to communicate effectively with the client. On the frontend, I worked particularly in leveraging specific aspects of AngularJS to enable features like lazy loading/infinite scrolling.',
-    links: ['pennypost.me', 'http://www.pennypost.me/', '', ''],
+    link: { address: 'http://www.pennypost.me/', text: 'pennypost.me' },
   },
+
   choicemap: {
     tab: { title: 'ChoiceMap', text: 'Collaborative work on frontend and API development', color: '#cccccc' },
     gif: './assets/images/choicemap.gif',
     text: 'ChoiceMap first asked me to execute a rebuild of their website. For this I worked off of provided comps, and collaborated on some design aspects. I then built the website entirely using HTML/CSS and JavaScript. I was later asked to assist on some backend tasks for their app, wherein I gained significant experience in collaborating on API development.',
-    links: ['choicemap.co', 'http://choicemap.co/', '', ''],
+    link: { address: 'http://choicemap.co/', text: 'choicemap.co' },
   },
+
   science: {
-    tab: { title: 'Experience the Science', text: 'Python/Flask, React/Materialize, Neil Tyson/Gif', color: '#00838f;' },
+    tab: { title: 'Experience the Science', text: 'Python/Flask, React/Materialize, Neil Tyson/Gif', color: '#00838f' },
     gif: './assets/images/tyson.png',
     text: 'Experience the Science was a project a friend and I created during a hackathon for women in software engineering. We made it in order to learn more about React and applying Google\'s Material Design in React\'s context. The site is served on Apache and its backend is written in Python/Flask. It utilizes the Giphy api along with a library of Neil DeGrasse Tyson quotes to pair a quote with a gif.',
-    links: ['experiencethescience.com', 'http://experiencethescience.com/', 'github.com/AraLawrence/wehack', 'https://github.com/AraLawrence/wehack'],
+    link: { address: 'http://experiencethescience.com/', text: 'experiencethescience.com', github: 'https://github.com/AraLawrence/wehack' },
   },
 };
 
